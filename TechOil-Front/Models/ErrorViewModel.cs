6b2295944f0ff732a75331d549df7f5a87cdd46d
lvingman <r_martin@tutanota.com>
2023-09-26 17:00:37 -0300

    Primer Commit

---
+++ TechOil-Front/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechOilFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
